f36847a97015ae0209471b603448d60994f93158
Emanuel Ramirez <emanuelramirez169@gmail.com>
2024-05-13 16:39:36 -0300

    Primer problema resuelto

---
+++ Problema 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Si enumeramos todos los números naturales a continuación 10 que son múltiplos de 3 o 5, obtenemos 3,5,6 y 9. La suma de estos múltiplos es 23 
+// Halla la suma de todos los múltiplos de 3 o 5 abajo 1000
+
+using System.Runtime.CompilerServices;
+
+var list = new List<int>();
+
+for (var i = 0; i < 1000; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        list.Add(i);
+    }
+}
+Console.WriteLine("La suma de los numeros naturales que son multiplos de 3 y 5 hasta el numero 1000 es: " + list.Sum());
+
